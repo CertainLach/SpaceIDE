a10acfd9078cd069b967364315946dcb01a15e76
BuildTools <iam@f6cf.pw>
2017-06-21 16:30:45 +0500

    Cleanup and reformat entry points

--- entryPoints/index.browser.js
+++ entryPoints/index.browser.js
@@ -1,15 +1,13 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Root from '../containers/Root';
-import {AppContainer} from 'react-hot-loader';
-import App from '../containers/App';
-import {createClientStore, createStore} from '../stores';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Root from "../containers/Root";
+import {createClientStore, createStore} from "../stores";
 
 const store = createClientStore(createStore());
 
 const render = (Root, container = document.getElementById('App')) => {
     ReactDOM.render(
-        React.createElement(Root,{stores:store}),
+        React.createElement(Root, {stores: store}),
         container,
     );
 };
--- entryPoints/index.node.js
+++ entryPoints/index.node.js
@@ -1,19 +1,21 @@
-import * as React from 'react';
-// Woop
-import * as ReactDOMServer from 'react-dom/server';
-import Root from '../containers/Root';
-import App from '../containers/App';
-import {createServerStore, createStore} from '../stores';
+import * as React from "react";
+import * as ReactDOMServer from "react-dom/server";
+import Root from "../containers/Root";
+import {createServerStore, createStore} from "../stores";
 
 
-export async function render(req){
+export async function render(req) {
     let store = createStore();
     const routerContext = {};
-    const renderedPage = await ReactDOMServer.renderToString(React.createElement(Root,{req,context:routerContext,stores:store}));
+    const renderedPage = await ReactDOMServer.renderToString(React.createElement(Root, {
+        req,
+        context: routerContext,
+        stores: store
+    }));
     const serverStore = createServerStore(store);
     return ({
         renderedPage,
         serverStore,
         routerContext
     });
-};
+}
